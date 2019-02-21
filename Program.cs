7a2dfddfbc6b094191a78896bd573282c49269c4
1martynas123 <33670849+1martynas123@users.noreply.github.com>
2019-02-22 00:48:27 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,18 +5,31 @@ namespace ConsoleApp1
   
     class Program
     {
-        public static int plusOne = 0;
+        public static int numberOfContact = 0;
+        public static string[] Contact = new string[100];
         public static string[] Name = new string[100];
+        public static string[] Surname = new string[100];
+        public static string[] Phone = new string[100];
+        public static int once = 0;
 
 
         static void Main()
         {
-            Console.WriteLine("Welcome, to PhoneBook program");
-            Help();
+           
+            once = once + 1;
+            if (once < 2)
+            {
+                Console.WriteLine("Welcome, to PhoneBook program \n");
+                Help();
+            }
             string choice = Console.ReadLine();
             if (choice == "Add")
             {
                 Add();
+            }
+            if (choice == "Find")
+            {
+                Find();
             }
             if (choice == "Remove")
             {
@@ -30,52 +43,94 @@ namespace ConsoleApp1
             {
                 Help();
             }
+            else {
+                  Main();
+            }
             Console.ReadLine();
         }
 
-       
 
+        //Create new Contact part
         static void Add()
-        {    //Create new Contact part
-              plusOne = plusOne + 1;
+        {    
+            numberOfContact = numberOfContact + 1;
            
             Console.WriteLine("Iveskite Varda");
-            Name[plusOne] = Console.ReadLine();
+            Name[numberOfContact] = Console.ReadLine();
+            Console.WriteLine("Iveskite Pavarde");
+            Surname[numberOfContact] = Console.ReadLine();
+            Console.WriteLine("Iveskite Telefono numeri");
+            Phone[numberOfContact] = Console.ReadLine();
+            Contact[numberOfContact] = "\n" + Name[numberOfContact] +" "
+                + Surname[numberOfContact]+"\n" + Phone[numberOfContact];
+            Console.WriteLine(Contact[numberOfContact]);
             Main();
-
-
-            Console.WriteLine("You chose to add new Contact");
         }
-        static void Remove()
+        //Find existing Contact part
+        static void Find()
         {
-            Console.WriteLine("You chose to Remove existing Contact");
-            //Remove existing Contact part
-        }
-        static void View()
-        {
-            Console.WriteLine("You chose to View all Contacts");
-            Console.WriteLine("You have "+ plusOne + "contacts");
-            Console.WriteLine("You chose to View all Contacts");
+            Console.WriteLine("Find by name or surname: ");
+            string search = Console.ReadLine();
             int i = 0;
-
             do
             {
                 i = i + 1;
-                Console.WriteLine(Name[i]);
-            } while (i != plusOne);
+                if ((search == Name[i]) || (search == Surname[i]))
+                {
+                    Console.WriteLine(Contact[i]);
+                    i = numberOfContact;
+                    Main();
+                }
+            } while (i != numberOfContact);
+          
+                Console.WriteLine("0 Matches Found by the name " + search + "\n");
+                Help();
+            
+
+
+
+
+
+        }
+        //Remove existing Contact part
+        static void Remove()
+        {
+            Console.WriteLine("Delete Contact by name or surname: ");
+            string search = Console.ReadLine();
+            int i = 0;
+            do
+            {
+                i = i + 1;
+                if ((search == Name[i]) || (search == Surname[i]))
+                {
+                    Console.WriteLine(Contact[i]+ "Is deleted");
+                    Contact[i] = null;
+                    i = numberOfContact;
+                    Main();
+                }
+            } while (i != numberOfContact);
+          
+                Console.WriteLine("0 Matches Found by the name " + search + "\n");
+                Help();     
+        }
+        //View all Contacts part
+        static void View()
+        {
+            int i = 0;
+            do
+            {
+                i = i + 1;
+                Console.WriteLine(Contact[i]);
+            } while (i != numberOfContact);
             Main();
-
-
-
-
-            //View all Contacts part
         }
         static void Help()
         {
             Console.WriteLine("To Add new contact, type (Add)");
+            Console.WriteLine("To Find existing contact, type (Find)");
             Console.WriteLine("To Remove existing contact, type (Remove)");
             Console.WriteLine("To view all contacts, type (View)");
-            Console.WriteLine("if you want to see help menu, type (Help)");
+            Console.WriteLine("if you want to see help menu, type (Help) \n");
         }
     
     }
