0023d7fbc60b598abed9aa605dc92d992a3998c6
1martynas123 <33670849+1martynas123@users.noreply.github.com>
2019-02-21 21:47:45 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,17 +1,82 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+  
     class Program
     {
+        public static int plusOne = 0;
+        public static string[] Name = new string[100];
+
+
         static void Main()
         {
-            Console.WriteLine("Hello, new Git user");
+            Console.WriteLine("Welcome, to PhoneBook program");
+            Help();
+            string choice = Console.ReadLine();
+            if (choice == "Add")
+            {
+                Add();
+            }
+            if (choice == "Remove")
+            {
+                Remove();
+            }
+            if (choice == "View")
+            {
+                View();
+            }
+            if (choice == "Help")
+            {
+                Help();
+            }
             Console.ReadLine();
         }
+
+       
+
+        static void Add()
+        {    //Create new Contact part
+              plusOne = plusOne + 1;
+           
+            Console.WriteLine("Iveskite Varda");
+            Name[plusOne] = Console.ReadLine();
+            Main();
+
+
+            Console.WriteLine("You chose to add new Contact");
+        }
+        static void Remove()
+        {
+            Console.WriteLine("You chose to Remove existing Contact");
+            //Remove existing Contact part
+        }
+        static void View()
+        {
+            Console.WriteLine("You chose to View all Contacts");
+            Console.WriteLine("You have "+ plusOne + "contacts");
+            Console.WriteLine("You chose to View all Contacts");
+            int i = 0;
+
+            do
+            {
+                i = i + 1;
+                Console.WriteLine(Name[i]);
+            } while (i != plusOne);
+            Main();
+
+
+
+
+            //View all Contacts part
+        }
+        static void Help()
+        {
+            Console.WriteLine("To Add new contact, type (Add)");
+            Console.WriteLine("To Remove existing contact, type (Remove)");
+            Console.WriteLine("To view all contacts, type (View)");
+            Console.WriteLine("if you want to see help menu, type (Help)");
+        }
+    
     }
 }
